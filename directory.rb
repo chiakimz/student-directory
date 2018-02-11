06b1eea42b2cbc763721a6f25e1f13100691bb1a
chiaki <chiakimizuta61@gmail.com>
2018-02-11 13:51:22 +0000

    Add comments and number of students

--- directory.rb
+++ directory.rb
@@ -1,4 +1,4 @@
-puts "The students of Villains Academy"
+#first we pring the list of students
 puts "-------------"
 puts "Dr. Hannibal Lecter"
 puts "Darth Vader"
@@ -11,4 +11,8 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
-	
+# finally, we print the total number of students
+print "Overall, we have "
+#it’s important that print() doesn’t add new line characters
+print 11
+print "great students"
